9a3f9f7532563a99c1899c1e222165946a08193e
Alfred <alfred523@gmail.com>
2015-03-02 11:54:00 -0800

    Major overhaul to the way weapons are selected, added player actor, SSG cannot be switched to via numbers. Various damage adjustments.

--- generate-pk3.sh
+++ generate-pk3.sh
@@ -6,5 +6,5 @@ if [ -f balanced_weapons.pk3 ]; then
 	rm -f balanced_weapons.pk3
 fi
 
-zip -r9 balanced_weapons.pk3 $FILES -x *.sh *.pk3
+zip -r9 balanced_weapons.pk3 $FILES -x *.sh *.pk3 *.backup* *.dbs*
 
